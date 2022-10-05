8ec1f931998c48610df4bfface37d6334e132ac9
Vu Tran <46880513+BlueBirdxx@users.noreply.github.com>
2022-10-05 16:55:06 +0700

    VN search

--- BlueBirdCoffeeAPI/Service/Services/ItemService.cs
+++ BlueBirdCoffeeAPI/Service/Services/ItemService.cs
@@ -40,10 +40,12 @@ namespace Service.Services
         {
             var data = _dbContext.Items
                 .Where(x => categoryId == null || x.CategoryId == categoryId)
-                .Where(x => string.IsNullOrEmpty(name) || x.Name.Contains(name))
                 .Where(x => id == null || x.Id == id)
                 .Where(x => x.IsDeleted == false)
                 .ToList();
+
+            data = data.Where(f => string.IsNullOrEmpty(name) || RemoveVNChar(f.Name).ToLower().Contains(name.ToLower()) || f.Name.ToLower().Contains(name.ToLower())).ToList();
+
             var result = _mapper.Map<List<ItemViewModel>>(data);
 
             foreach (var item in result)
@@ -56,6 +58,33 @@ namespace Service.Services
 
             return result;
         }
+
+        private static readonly string[] VietNamChar = new string[]{
+        "aAeEoOuUiIdDyY",
+        "áàạảãâấầậẩẫăắằặẳẵ",
+        "ÁÀẠẢÃÂẤẦẬẨẪĂẮẰẶẲẴ",
+        "éèẹẻẽêếềệểễ",
+        "ÉÈẸẺẼÊẾỀỆỂỄ",
+        "óòọỏõôốồộổỗơớờợởỡ",
+        "ÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠ",
+        "úùụủũưứừựửữ",
+        "ÚÙỤỦŨƯỨỪỰỬỮ",
+        "íìịỉĩ",
+        "ÍÌỊỈĨ",
+        "đ",
+        "Đ",
+        "ýỳỵỷỹ",
+        "ÝỲỴỶỸ"};
+        public static string RemoveVNChar(string str)
+        {
+            for (int i = 1; i < VietNamChar.Length; i++)
+            {
+                for (int j = 0; j < VietNamChar[i].Length; j++)
+                    str = str.Replace(VietNamChar[i][j], VietNamChar[0][i - 1]);
+            }
+            return str;
+        }
+
         public Guid Add(ItemAddModel model)
         {
             var data = _mapper.Map<ItemAddModel, Item>(model);
