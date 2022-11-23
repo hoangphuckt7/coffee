445682dcbaa744feb13592f0b0b974582eae96c3
Vu Tran <46880513+BlueBirdxx@users.noreply.github.com>
2022-11-23 23:59:26 +0700

    ok

--- AdminManager/AdminManager/Controllers/BillController.cs
+++ AdminManager/AdminManager/Controllers/BillController.cs
@@ -8,13 +8,30 @@ namespace AdminManager.Controllers
 {
     public class BillController : Controller
     {
-        public async Task<IActionResult> Index(DateTime? singleDate)
+        public async Task<IActionResult> Index(DateTime? singleDate, DateTime? fromDate, DateTime? toDate, int? pageSize = 10, int? pageIndex = 1, bool? isNewest = true)
         {
-            string path = "api/Bill/ExportData?isNewest=true";
+            string path = "api/Bill/ExportData?isNewest=" + isNewest;
             if (singleDate != null)
             {
                 path += "&date=" + singleDate;
             }
+            if (pageSize != null)
+            {
+                if (pageSize != -1) path += "&pageSize=" + pageSize;
+            }
+
+            if (fromDate != null)
+            {
+                path += "&fromDate=" + fromDate;
+            }
+
+            if (toDate != null)
+            {
+                path += "&toDate=" + toDate;
+            }
+
+            path += "&pageIndex=" + pageIndex;
+
             var rawData = await ApiBuilder.SendRequest<object>(path, null, RequestMethod.GET, true, Request.GetDisplayUrl(), HttpContext.Session);
 
             var data = await ApiBuilder.ParseToData<BilPagingModel>(rawData);
@@ -22,7 +39,17 @@ namespace AdminManager.Controllers
 
             ViewBag.Income = data.Income;
             ViewBag.EstimateIncome = data.EstimateIncome;
+
             ViewBag.CurrentDate = singleDate;
+            ViewBag.PageSize = pageSize;
+            ViewBag.IsNewest = isNewest;
+
+            ViewBag.FromDate = fromDate;
+            ViewBag.ToDate = toDate;
+
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPage = Math.Ceiling((decimal)data.Total / (decimal)pageSize!.Value);
+
             return View();
         }
     }
